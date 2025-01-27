76ff9e13dbf55cbfa3004d9a5efbd21db0f44e0f
Arttu Ronkainen <arttu.ronkainen@centraali.fi>
2025-01-27 18:28:06 +0200

    Parannettu responsiivisuus1

--- scripts.js
+++ scripts.js
@@ -2,7 +2,7 @@ document.querySelectorAll("nav a").forEach(anchor => {
     anchor.addEventListener("click", function (e) {
         e.preventDefault();
         document.querySelector(this.getAttribute("href")).scrollIntoView({
-            behavior: "smooth" 
+            behavior: "smooth"
         });
     });
 });
